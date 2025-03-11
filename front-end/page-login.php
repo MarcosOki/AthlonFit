e44779754c1a14bf2bc5921d33923ba97904ec9e
Daniel-Marim <daniel.amadormarim@gmail.com>
2025-03-11 11:19:34 -0300

    add de fonte na pagina

--- front-end/page-login.php
+++ front-end/page-login.php
@@ -21,9 +21,8 @@ if (isset($_GET["erro"])) {
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
-    <style>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&display=swap" rel="stylesheet">
 
-    </style>
 </head>
 
 <body>
